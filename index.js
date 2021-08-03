4cce36419dbbc66ee1b231916ca9a574b87f9182
Aleksandr <aleksandr.logvinov@foodpanda.co.jp>
2021-08-03 21:25:41 +0900

    working example

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 const {BrowserWindow, app} = require('electron');
-const path = require("path");
+// include the Node.js 'path' module at the top of your file
+const path = require('path')
 
 let win;
 
 let createWindow = () => {
     win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 800,
         // frame: false
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false,
         }
 
     });
-
     // win.loadURL(`file://${__dirname}/index.html`);
     //win.webContents.openDevTools();
     win.loadFile(`index.html`);
+    win.webContents.openDevTools()
+    return win
 };
 
 app.whenReady().then(() => {
     createWindow()
-
-    // downloadAs(
-    //     "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png",
-    //     "name",
-    // )
-    //     .then(r => {
-    //     })
-    //     .catch(e => {
-    //         throw e
-    //     })
+        .then(win => downloadAs(
+                win,
+                "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png",
+                "name",
+            )
+        )
+        .catch(e => {
+            throw e
+        })
 
     app.on('activate', function () {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
@@ -41,16 +44,16 @@ app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
 })
 
-// downloadAs = async (url) => {
-//     const blob = await axios.get(url, {
-//         headers: {
-//             'Content-Type': 'application/octet-stream',
-//         },
-//         responseType: 'blob',
-//     });
-//     const a = document.createElement('a');
-//     const href = window.URL.createObjectURL(blob.data);
-//     a.href = href;
-//     a.download = "name";
-//     // a.click();
-// };
+downloadAs = async (win, url, name) => {
+    debugger;
+    const blob = await axios.get(url, {
+        headers: {
+            'Content-Type': 'application/octet-stream',
+        },
+        responseType: 'blob',
+    });
+    const a = win.document.createElement('a');
+    a.href = win.URL.createObjectURL(blob.data);
+    a.download = name;
+    // a.click();
+};
--- script.js
+++ script.js
@@ -1,149 +1,58 @@
-var app = angular.module('app', ['ngRoute']);
+const axios = require("axios");
+const fs = require("fs");
+// /thumbnail?fname=DSC_3144.JPG&fdir=100D3300&ftype=0&time=1392668307
+// /download?file=DCIM%5C100D3300%5CDSC_3080.JPG
+// /thumbnail?fname=DSC_3143.JPG&fdir=100D3300&ftype=0&time=1392668302
+// /download?file=DCIM%5C100D3300%5CDSC_3081.JPG
+downloadAs = async (url, name) => {
+    const img = document.getElementById('down-image');
+    img.src = url;
+    const blob = await axios.get(url, {
+        headers: {
+            'Content-Type': 'application/octet-stream',
+        },
+        responseType: 'blob',
+    });
+    img.src = window.URL.createObjectURL(blob.data);
+    // img.download = name;
+    // img.click();
+};
 
-const {remote} = require('electron');
+// todo add auto discovery
+const input = "http://ezshare.card/dir?dir=A:%5CDCIM%5C100D3300";
 
-app.service('image', function() {
-	var imagePath = "";
-	var dimesions = [];
-	this.setImagePath = function(path) {
-		imagePath = path;
-	};
-	this.getImagePath = function() {
-		return imagePath;
-	};
-	this.setImageDimensions = function(imgDimesions) {
-		dimesions = imgDimesions;
-	};
-	this.getImageDimensions = function() {
-		return dimesions;
-	};
-});
+function start() {
+    const imgPrev = document.getElementById('prev-image');
+    const imgFull = document.getElementById('full-image');
 
-app.config(function($routeProvider) {
-	$routeProvider.when('/', {
-		templateUrl: `${__dirname}/components/home/home.html`,
-		controller: 'homeCtrl'
-	}).when('/edit', {
-		templateUrl: `${__dirname}/components/editImage/editImage.html`,
-		controller: 'editCtrl'
-	}).otherwise({
-		template: '404 bro'
-	});
-});
+    axios.get(input)
+        .then((response) => {
+            if (response.status !== 200) {
+                throw new Error("not 200 response")
+            }
+            const html = response.data;
 
-app.controller('headCtrl', function($scope) {
-	var win = remote.getCurrentWindow();
+            const newDiv = document.createElement("div");
+            newDiv.innerHTML = html;
+            const allElementsUnderPreA = document.evaluate("//pre/a", newDiv, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 
-	$scope.close = function() {
-		win.close();
-	};
-	$scope.maximize = function() {
-		win.isMaximized() ? win.unmaximize() : win.maximize();
-	};
-	$scope.minimize = function() {
-		win.minimize();
-	}
-});
+            for (let i = 0, l = allElementsUnderPreA.snapshotLength; i < l; i++) {
+                let el = allElementsUnderPreA.snapshotItem(i)
+                if (el.href &&
+                    el.href.includes("download") &&
+                    el.href.includes("JPG")
+                ) {
+                    imgPrev.src = el.href.replace("download", "thumbnail");
+                    imgFull.src = el.href;
+                    return
+                }
+            }
+        })
+        .catch((err) => {
+            throw new Error(err);
+        });
+}
 
-app.controller('homeCtrl', function($scope, $location, image) {
-	$scope.pickFile = function() {
-		var {dialog} = remote;
-		dialog.showOpenDialog({
-			properties: ['openFile'],
-			filters: [{
-				name: 'Images',
-				extensions: ['jpg', 'jpeg', 'png']
-			}]
-		}, function(file) {
-			if(!!file) {
-				var path = file[0];
-				image.setImagePath(path);
-				var sizeof = require('image-size');
-
-				var dimesions = sizeof(path); // dimesions.width and dimesions.height
-
-				image.setImageDimensions(dimesions);
-				$location.path('/edit');
-				$scope.$apply();
-			}
-		});
-	};
-});
-
-app.controller('editCtrl', function($scope, image, $location) {
-	$scope.imagePath = image.getImagePath();
-
-	$scope.controlsActive = false;
-
-	var imageReference = document.getElementById('mainImage');
-
-	var generatedStyles = "";
-
-	$scope.effects = {
-		'Brightness': {val: 100, min: 0, max:200, delim: '%'}, 
-		'Contrast': {val: 100, min: 0, max:200, delim: '%'}, 
-		'Invert': {val: 0, min: 0, max:100, delim: '%'}, 
-		'Hue-Rotate': {val: 0, min: 0, max:360, delim: 'deg'}, 
-		'Sepia': {val: 0, min: 0, max:100, delim: '%'}, 
-		'Grayscale': {val: 0, min: 0, max:100, delim: '%'}, 
-		'Saturate': {val: 100, min: 0, max:200, delim: '%'}, 
-		'Blur': {val: 0, min: 0, max:5, delim: 'px'}
-	};
-	
-	$scope.imageEffect = function(effectName) {
-		$scope.controlsActive = true;
-		$scope.activeEffect = effectName;
-		console.log(effectName);
-	};
-
-	$scope.setEffect = function() {
-		generatedStyles = "";
-		for(let i in $scope.effects) { // i = Brightness and $scope.effects[i].val
-			generatedStyles += `${i}(${$scope.effects[i].val+$scope.effects[i].delim}) `;
-		}
-		imageReference.style.filter = generatedStyles;
-		console.log(generatedStyles);
-	}
-
-	$scope.hideThis = function() {
-		$scope.controlsActive = false;
-	};
-
-	$scope.change = function() {
-		$location.path('/');
-	}
-
-	$scope.save = function() {
-		// the magic goes
-		const {BrowserWindow} = remote;
-		var dimesions = image.getImageDimensions();
-		let src = image.getImagePath();
-
-		let styles = imageReference.style.filter;
-
-		let win = new BrowserWindow({
-			frame: false,
-			show: false,
-			width: dimesions.width,
-			height: dimesions.height,
-			webPreferences: {
-				webSecurity: false
-			}
-		});
-
-		win.loadURL(`data:text/html,
-			<style>*{margin:0;padding:0;}</style><img src="${src}" style="filter: ${styles}">
-			
-			<script>
-				var screenshot = require('electron-screenshot');
-				screenshot({
-					filename: 'userFile.png',
-					delay: 1000
-				});
-			</script>
-		
-			`);
-
-	};
-
-});
+setInterval(function () {
+    start();
+}, 1000);
